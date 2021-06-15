61bd5298d9f78bde7a78f6e0d72b2cfe20f8d2c2
Mai Kawauchi <49463452+MaiKawauchi@users.noreply.github.com>
2021-06-15 23:56:56 +0900

    Add files via upload

---
+++ file_io.hpp
@@ -0,0 +1,36 @@
+#pragma
+
+#include <fstream>
+#include <vector>
+std::vector<char> ReadFile(const char *filename, std::string *log) {
+
+    std::ifstream ifs(filename, std::ios::binary);
+    if (!ifs.is_open()) {
+        *log = "Can not open " + std::string{filename} + ".";
+        return {};
+    }
+
+    // ファイルサイズ取得
+    ifs.seekg(0, std::ios::end);
+    auto size = ifs.tellg();
+    ifs.seekg(0);
+
+    // fixme: あまりにもサイズがでかすぎると new で確保できない場合がある
+    std::vector<char> data(size);
+    ifs.read(data.data(), size);
+
+    return data;
+}
+
+// ---
+
+void WriteFile(const char* filename, const std::vector<char>& data)
+{
+    std::ofstream ofs(filename, std::ios::binary);
+    if(!ofs.is_open())
+    {
+        // fixme: 対応する
+    }
+    ofs.write(data.data(), data.size());
+    ofs.close();
+}
